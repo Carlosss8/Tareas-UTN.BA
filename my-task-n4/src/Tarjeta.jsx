e2e0c30d8f4bc5de3a48dccc3d9d68d190c83dbb
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2025-12-19 21:49:53 -0300

    Editing CSS ✔

--- my-task-n4/src/Tarjeta.jsx
+++ my-task-n4/src/Tarjeta.jsx
@@ -1,11 +1,16 @@
+import "./Tarjeta.css"
+
 const Tarjeta = ({ nombre, profesion, imagen, descripcion }) => {
     return (
-        <div className="tarjeta">
-            <img src={imagen} alt={nombre} width="180" />
-            <h2>{nombre}</h2>
-            <h3>{profesion}</h3>
-            <p>{descripcion}</p>
-        </div>
+        <main>
+            <div className="tarjeta">
+                <img src={imagen} alt={nombre} width="180" />
+                <h2>{nombre}</h2>
+                <h3>{profesion}</h3>
+                <p>{descripcion}</p>
+            </div>
+        </main>
+
     )
 }
 
