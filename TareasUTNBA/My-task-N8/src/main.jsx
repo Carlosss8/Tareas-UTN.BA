414ddf6cb91bb4146a9686a0fc22560ba19bf2a6
CarlossRodriguez8 <carlosrodriguez8ra@gmail.com>
2026-01-14 21:40:49 -0300

    point 5 ✔️new form with "react-hook-form"

--- TareasUTNBA/My-task-N8/src/main.jsx
+++ TareasUTNBA/My-task-N8/src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Forms } from './views/forms'
+import { FormsReact } from './views/reactHookForm'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <h1 style={{ textAlign: "center" }}>Formulario sin react-hook-form</h1>
     <Forms />
-  </StrictMode>,
+    <h1 style={{ textAlign: "center" }}>Formulario con react-hook-form</h1>
+    <FormsReact />
+  </StrictMode>
 )
